1f4cb0ecce9266719eb10527b3c3ec34df853b8c
19ucs117 <77544652+19ucs117@users.noreply.github.com>
2022-01-15 13:42:48 +0530

    Update 2021_12_24_114219_create_registration_third_stages_table.php

--- divyangJanAPI/database/migrations/2021_12_24_114219_create_registration_third_stages_table.php
+++ divyangJanAPI/database/migrations/2021_12_24_114219_create_registration_third_stages_table.php
@@ -16,12 +16,12 @@ class CreateRegistrationThirdStagesTable extends Migration
         Schema::create('registration_third_stages', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->uuid('handicapped_person_id');
-            $table->text('railway_concession_certificate')->nullable();
-            $table->text('disability_or_handicapped_certificate')->nullable();
-            $table->text('age_certificate')->nullable();
-            $table->text('aadhaar_card')->nullable();
-            $table->text('address_proof')->nullable();
-            $table->text('passportsize_photo')->nullable();
+            $table->text('railway_concession_certificate');
+            $table->text('disability_or_handicapped_certificate');
+            $table->text('age_certificate');
+            $table->text('aadhaar_card');
+            $table->text('address_proof');
+            $table->text('passportsize_photo');
             $table->timestamps();
             $table->foreign('handicapped_person_id')->references('id')->on('users')
                                                                         ->onUpdate('cascade')
